b36cc11f57b7b7149a6b64cbf8a35b0b36c80e1e
Liang Shining <shiningliang@users.noreply.github.com>
2022-05-02 10:07:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import requests, json, time, re, argparse
+import argparse
+import json
+import re
+import requests
+import time
 from bs4 import BeautifulSoup
 
 login_url = 'https://ehall.jlu.edu.cn/sso/login'
@@ -7,7 +11,8 @@ start_url = 'https://ehall.jlu.edu.cn/infoplus/interface/start'
 render_url = 'https://ehall.jlu.edu.cn/infoplus/interface/render'
 action_url = 'https://ehall.jlu.edu.cn/infoplus/interface/doAction'
 
-UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36 Edg/87.0.664.47'
+UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.67 Safari/537.36 Edge/87.0.664.47'
+
 
 def check(username, password):
     for _ in range(10):
@@ -16,32 +21,32 @@ def check(username, password):
 
             headers = {'User-Agent': UA}
 
-            #获取pid
+            # 获取pid
             login_html = s.get(url=login_url, headers=headers)
             soup = BeautifulSoup(login_html.text, 'lxml')
-            pid = soup.find(name="input", attrs={"name" :"pid"}).get('value')
+            pid = soup.find(name="input", attrs={"name": "pid"}).get('value')
 
-            #登录
+            # 登录
             data = {'username': username, 'password': password, 'pid': pid, 'source': ''}
             s.post(url=login_url, data=data, headers=headers)
 
-            #获取csrf_token
+            # 获取csrf_token
             form_html = s.get(url=form_url, headers=headers)
             soup = BeautifulSoup(form_html.text, 'lxml')
-            csrf_token = soup.find(name="meta", attrs={"itemscope" :"csrfToken"}).get('content')
+            csrf_token = soup.find(name="meta", attrs={"itemscope": "csrfToken"}).get('content')
 
-            #获取打卡地址
-            headers = {'User-Agent': UA,'Referer': form_url}
+            # 获取打卡地址
+            headers = {'User-Agent': UA, 'Referer': form_url}
             data = {'idc': 'YJSMRDK', 'csrfToken': csrf_token}
             start_json = s.post(url=start_url, data=data, headers=headers)
             step_id = re.search('(?<=form/)\\d*(?=/render)', start_json.text)[0]
 
-            #获取打卡基础信息
+            # 获取打卡基础信息
             data = {'stepId': step_id, 'csrfToken': csrf_token}
             render = s.post(url=render_url, data=data, headers=headers)
             render_info = json.loads(render.content)['entities'][0]['data']
 
-            #打卡
+            # 打卡
             data = {
                 'actionId': 1,
                 'formData': json.dumps(render_info),
@@ -78,5 +83,6 @@ if __name__ == "__main__":
         check(username=args.user, password=args.pwd)
     else:
         from users import *
+
         for user in users:
             check(user['username'], user['password'])
